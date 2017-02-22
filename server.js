e0e0437fe33306ddb6ccf55b628ce7caa9627eac
adamos486 <adamos486@gmail.com>
2017-02-21 21:09:25 -0800

    Getting rid of non-generic comments

--- server.js
+++ server.js
@@ -1,9 +1,4 @@
 var express = require('express');
-// import injectTapEventPlugin from 'react-tap-event-plugin';
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-// injectTapEventPlugin();
 
 //Create our app
 var app = express();
