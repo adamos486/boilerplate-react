abe09d5d357c2396e70a5b2d93731fae8d7e474a
adamos486 <adamos486@gmail.com>
2017-02-21 21:11:30 -0800

    Changing port and making it a variable

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var port = 3010;
 
 //Create our app
 var app = express();
 
 app.use(express.static('public'));
 
-app.listen(8080, function() {
-  console.log('Express server is up on port 8080');
+app.listen(port, function() {
+  console.log(`Express server is up on port ${port}`);
 });
